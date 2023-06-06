00b70ae2a7b24ead614698df4bfcd6a8eedc6ca7
Phong Thieu <pthieu@gmail.com>
2023-06-05 22:20:26 -0400

    DB: add drizzle, migration files, migrate before server start

--- next.config.js
+++ next.config.js
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
+
+const { drizzle } = require('drizzle-orm/node-postgres');
+const { migrate } = require('drizzle-orm/node-postgres/migrator');
+const { resolve } = require('node:path');
+
+const { Client } = require('pg');
+
+
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = async (phase) => {
+  if (
+    phase === 'phase-production-server' &&
+    process.env.ENVIRONMENT === 'production'
+  ) {
+    console.log(`Retrieving secrets from AWS SSM Parameter Store...`);
+    // TODO: Get secrets code
+    console.log(`Successfully updated process.env with secrets`);
+  }
+
+  console.log('Running migrations...');
+  await migrateLatest()
+  console.log('Migrations completed successfully');
+
+  return nextConfig;
+};
+
+async function migrateLatest() {
+  console.log('Running migrations...');
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  await client.connect()
+  const db = drizzle(client)
+  await migrate(db, { migrationsFolder: resolve(__dirname, 'src', 'db', 'migrations') });
+  console.log('Migrations completed successfully');
+  client.end();
+}
